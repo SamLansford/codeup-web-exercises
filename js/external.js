61d5feef3a373b91b413c81eb952df0a98d86f64
Sam Lansford <sam.j.lansford@gmail.com>
2020-12-14 17:33:49 -0600

    All done with exercises

--- js/external.js
+++ js/external.js
@@ -1,13 +1,13 @@
-"use strict"
-console.log("Hello from external JavaScript")
-alert('Welcome to my Website!')
-
-let person1 = {
-    firstName: "Sam",
-    lastName: "Lansford",
-}
-person1.email = prompt("Enter your email")
-alert("Welcome," + person1.firstName + ' ' + person1.lastName)
+// "use strict"
+// console.log("Hello from external JavaScript")
+// alert('Welcome to my Website!')
+//
+// let person1 = {
+//     firstName: "Sam",
+//     lastName: "Lansford",
+// }
+// person1.email = prompt("Enter your email")
+// alert("Welcome," + person1.firstName + ' ' + person1.lastName)
 
 // var respond = prompt('What is your favorite color');
 // alert("Great," + respond + " is my favorite color too!");
@@ -39,7 +39,50 @@ alert("Welcome," + person1.firstName + ' ' + person1.lastName)
 // var isPremiumMember = confirm("Are you a premium member?")
 // alert("is the product offer applied: " + (isOfferValid + isPremiumMember))
 
-function Hello() {
-    alert("hey there");
+// function Hello() {
+//     alert("hey there");
+// }
+// Hello("codeup");
+
+function isFive(input) {
+    return input == 5;
 }
-Hello("codeup");
+
+console.log(isFive(5));
+console.log(isFive("5"));
+
+// Write a function, isShortWord, that takes in a string and returns the boolean value true
+// if the passed argument is shorter than 5 characters. Return false otherwise.
+
+function isShortWord(str) {
+    return str.length < 5;
+}
+
+console.log(isShortWord('hi'))
+console.log(isShortWord('everybody'))
+
+// Write a function, isSameLength, that takes in two string inputs and returns the boolean value true
+// if the passed arguments are the same length. Return false otherwise.
+
+function isSameLength(str1, str2) {
+    return str1.length === str2.length
+}
+
+console.log(isSameLength('me', 'me'));
+console.log(isSameLength('me', 'him'));
+console.log(isSameLength('hi', 'me'));
+
+// Write a function, getSmallerSegment, that takes in a string and a number input. The function should return
+// a substring of the first argument that is as many characters long as the second argument in lowercase.
+
+function getSmallerSegment(str, num) {
+    return str.substring(0, num).toLowerCase();
+}
+
+console.log(getSmallerSegment("codeup", 6));
+
+//IIFE syntax
+
+(function() {
+
+})();
