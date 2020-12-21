7e94212f6598447cd856aec6d9a9b44785d3ae79
Sam Lansford <sam.j.lansford@gmail.com>
2020-12-20 19:15:25 -0600

    feat: added more while loop

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,18 +1,18 @@
-'use strict'
-
-while (true) {
-    var number = prompt("Give me an odd number between 1-50");
-    if (number % 2 !== 0 && number <= 50 && number >= 1) {
-        break;
-    }
-}
-console.log('Number to skip is: ' + number);
-for (var i = 1; i <= 50; i+=2) {
-    if (i == number) {
-        console.log('Yikes! Skipping number: ' + i);
-        continue;
-    }
-    console.log('Here is an odd number: ' + i);
-}
+// 'use strict'
+//
+// while (true) {
+//     var number = prompt("Give me an odd number between 1-50");
+//     if (number % 2 !== 0 && number <= 50 && number >= 1) {
+//         break;
+//     }
+// }
+// console.log('Number to skip is: ' + number);
+// for (var i = 1; i <= 50; i+=2) {
+//     if (i == number) {
+//         console.log('Yikes! Skipping number: ' + i);
+//         continue;
+//     }
+//     console.log('Here is an odd number: ' + i);
+// }
 
 
--- js/for_loops.js
+++ js/for_loops.js
@@ -35,7 +35,7 @@
 // }
 
 
-for (var i = 1; i <= 100; i++) {
-    console.log(i);
-}
+// for (var i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
 
