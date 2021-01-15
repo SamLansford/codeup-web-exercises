94881c94e304ac890ed1afb0e0d401c5304bb918
Sam Lansford <sam.j.lansford@gmail.com>
2021-01-15 09:48:46 -0600

    js worm-up

--- js/planets-string.js
+++ js/planets-string.js
@@ -45,56 +45,56 @@
 // Define a function named allIndexesOf that takes in two arguments. The first argument should be the array to search and the second
 // argument should be the value you want to search for. If the item does not exist in the provided array, return an empty array.
 
-var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
-
-function allIndexesOf(array, value) {
-    let returnArray = [];
-    array.forEach(function(element, index, array){
-        if (element == value){
-            returnArray.push(index);
-        }
-    });
-    return returnArray;
-}
-console.log(allIndexesOf(fruits,"apple"));
-console.log(allIndexesOf(fruits, "guava"));
-console.log(allIndexesOf(fruits, "pineapple"));
+// var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+//
+// function allIndexesOf(array, value) {
+//     let returnArray = [];
+//     array.forEach(function(element, index, array){
+//         if (element == value){
+//             returnArray.push(index);
+//         }
+//     });
+//     return returnArray;
+// }
+// console.log(allIndexesOf(fruits,"apple"));
+// console.log(allIndexesOf(fruits, "guava"));
+// console.log(allIndexesOf(fruits, "pineapple"));
 
 // Define a function named removeAll(array, value) that takes in two arguments. The first argument should be an array and
 // the second argument should be a value you wish to remove
 
-let bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
-
-function removeAll(array, value) {
-    let returnArray = [];
-    array.forEach(function (element, index, array){
-        if (element !== value) {
-            returnArray.push(element);
-        }
-    });
-    return returnArray
-}
-
-console.log(removeAll(bugs, "ant"));
-console.log(removeAll(bugs, "mosquito"));
-console.log(removeAll(bugs, "roach"));
+// let bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+//
+// function removeAll(array, value) {
+//     let returnArray = [];
+//     array.forEach(function (element, index, array){
+//         if (element !== value) {
+//             returnArray.push(element);
+//         }
+//     });
+//     return returnArray
+// }
+//
+// console.log(removeAll(bugs, "ant"));
+// console.log(removeAll(bugs, "mosquito"));
+// console.log(removeAll(bugs, "roach"));
 
 
 // Make a function called randomIntBetween(min, max) that returns a random number between
 // the min and the max.
 
-function randomIntBetween(min, max) {
-    return Math.floor(Math.random() * (max + min + 1) ) + min;
-}
+// function randomIntBetween(min, max) {
+//     return Math.floor(Math.random() * (max + min + 1) ) + min;
+// }
 
-console.log("The Number between max and minimum is " + randomIntBetween(1,90));
+// console.log("The Number between max and minimum is " + randomIntBetween(1,90));
 
 // Make a function called coinFlip() that returns either 0 or 1, randomly
-
-function coinFlip() {
-    return Math.floor(Math.random() * 2);
-};
-console.log("You got " + coinFlip() + " on the coin flip");
+//
+// function coinFlip() {
+//     return Math.floor(Math.random() * 2);
+// };
+// console.log("You got " + coinFlip() + " on the coin flip");
 
 
 // Make a function called twoDice() that returns the sum of rolling two six sided dice.
@@ -146,8 +146,9 @@ function listOfRolls(num) {
         returnArray.push(rollDie());
     } return returnArray;
 };
-//let numberOfRolls = prompt("How many time would you like to roll the dice" );
-//console.log("Here are the result of the rolls " + listOfRolls(numberOfRolls));
+let numberOfRolls = prompt("How many time would you like to roll the dice" );
+console.log("Here are the result of the rolls " + listOfRolls(numberOfRolls));
+
 
 
 // Make a function called listOfRollsFromDieFunc(numberOfRolls, diceFunction)
