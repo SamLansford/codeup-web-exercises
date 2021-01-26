b83c813e829e12f2b19c9c4f404ef438c5aadd02
Sam Lansford <sam.j.lansford@gmail.com>
2021-01-26 15:59:24 -0600

    doing dom

--- js/morning-pushup.js
+++ js/morning-pushup.js
@@ -15,8 +15,20 @@ console.log(isFactor(2,5))
 
 // write a JavaScript function that takes in an array as an argument, and returns a new array containing the first and the last elements of the passed array.
 
-function firstLastOfArray(arr) {
+function firstLastArray(arr) {
     let bucket = [];
-    bucket.push(arr[0])
-    bucket
+    bucket.push(arr[0]);
+    bucket.push(arr[arr.length - 1]);
+    return bucket;
+}
+
+
+function firstLastArray(arr) {
+    let bucket = [];
+    for (let i = 0; i < arr.length; i+=1) {
+        if (arr[i].length < arr.length -1) {
+            bucket.push(arr[i])
+        }
+    }
+    return bucket;
 }
