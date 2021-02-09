8d75a56725e5828ce1bfb4ec841ac18179376a8f
Sam Lansford <sam.j.lansford@gmail.com>
2021-02-08 20:07:22 -0600

    done with the mapbox

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -25,3 +25,32 @@ function reverseGeocode(coordinates, token) {
         });
 }
 
+var favRestaurants = [
+    {
+        name: "huhot",
+        information: "huhot is a buffet where you pick your food and people cook it for you",
+        location: [-98.57591, 29.54685],
+    },
+    {
+        name: "KungFu noodles",
+        information: "Hand-pulled noodles, dumplings & " +
+        "steamed buns are the specialties at this modest Chinese restaurant.",
+        location: [-98.61865, 29.496402],
+    },
+    {
+        name: "ShiFu Noodles",
+        information: "ShiFu Noodle is a new Sichuan-style Chinese noodle house",
+        location: [-98.458924, 29.380408],
+    }
+]
+
+favRestaurants.forEach(function (restaurant){
+    let popup = new mapboxgl.Popup()
+        .setHTML(`<p>${restaurant.name}<br>${restaurant.information}</p>`)
+    new mapboxgl.Marker()
+        .setLngLat(restaurant.location)
+        .setPopup(popup)
+        .addTo(map)
+}
+
+)
