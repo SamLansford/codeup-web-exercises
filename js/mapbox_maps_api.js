2ba3a36403579eb0344f6b1a431e62e226cade48
Sam Lansford <sam.j.lansford@gmail.com>
2021-02-09 00:10:42 -0600

    adding an image

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -12,18 +12,18 @@ function geocode(search, token) {
         });
 }
 
-function reverseGeocode(coordinates, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
-        .then(function(res) {
-            return res.json();
-        })
-        // to get all the data from the request, comment out the following three lines...
-        .then(function(data) {
-            return data.features[0].place_name;
-        });
-}
+// function reverseGeocode(coordinates, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+//         .then(function(res) {
+//             return res.json();
+//         })
+//         // to get all the data from the request, comment out the following three lines...
+//         .then(function(data) {
+//             return data.features[0].place_name;
+//         });
+// }
 
 var favRestaurants = [
     {
@@ -44,11 +44,14 @@ var favRestaurants = [
     }
 ]
 
-// favRestaurants.forEach(function (restaurant){
-//     let popup = new mapboxgl.Popup()
-//         .setHTML(`<h3>${restaurant.name}</h3><br><p></p>${restaurant.information}</p>`)
-//     new mapboxgl.Marker()
-//         .setLngLat(restaurant.location)
-//         .setPopup(popup)
-//         .addTo(map)
-// })
+favRestaurants.forEach(function (restaurant){
+    let popup = new mapboxgl.Popup()
+        .setHTML('<h3>${restaurant.name}</h3><br></p>${restaurant.information}</p>' +
+            '<img src="img/huhot.png" width="50px">')
+    new mapboxgl.Marker()
+        .setLngLat(restaurant.location)
+        .setPopup(popup)
+        .addTo(map)
+})
+
+
