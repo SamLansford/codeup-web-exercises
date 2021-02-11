d01bb62026162bc9d20b5095824d026928155240
Sam Lansford <sam.j.lansford@gmail.com>
2021-02-11 17:18:10 -0600

    making changes to t-block

--- js/geocode.js
+++ js/geocode.js
@@ -12,6 +12,19 @@ function geocode(search, token) {
         });
 }
 
+// function reverseGeocode(coordinates, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+//         .then(function(res) {
+//             return res.json();
+//         })
+//         // to get all the data from the request, comment out the following three lines...
+//         .then(function(data) {
+//             return data.features[0].place_name;
+//         });
+// }
+
 function reverseGeocode(coordinates, token) {
     var baseUrl = 'https://api.mapbox.com';
     var endPoint = '/geocoding/v5/mapbox.places/';
@@ -21,6 +34,7 @@ function reverseGeocode(coordinates, token) {
         })
         // to get all the data from the request, comment out the following three lines...
         .then(function(data) {
+            console.log(data)
             return data.features[0].place_name;
         });
 }
