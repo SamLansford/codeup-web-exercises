0f1c3893894c6a108c0ee77a106b38e98a5ad407
Sam Lansford <sam.j.lansford@gmail.com>
2021-05-19 16:14:26 -0500

    writing js code to relearn it

--- js/warm-up.js
+++ js/warm-up.js
@@ -29,3 +29,12 @@ function average() {
     console.log(avg);
 }
 console.log(average());
+
+const x = 20;
+let answer;
+
+if (x > 10) {
+    answer = "greater than 10";
+} else {
+    answer =  "less than 10";
+}
